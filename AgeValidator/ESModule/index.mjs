13e27dde4d3a5efa91ae129d0936d937a7e92728
1101795 <sivasai.kuruva@ness.com>
2023-08-01 11:35:49 +0530

    Final Submission of node assignment

--- AgeValidator/ESModule/index.mjs
+++ AgeValidator/ESModule/index.mjs
@@ -1,29 +1,41 @@
 import calcuAge from "./calcuAge.mjs";
-import { createInterface } from "readline";
+// import { createInterface } from "readline";
 
-const readline = createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const readline = createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
-const readLineAsync = msg => {
-    return new Promise(resolve => {
-        readline.question(msg, userRes => {
-            resolve(userRes);
-        });
-    });    
+// const readLineAsync = msg => {
+//     return new Promise(resolve => {
+//         readline.question(msg, userRes => {
+//             resolve(userRes);
+//         });
+//     });
+// }
+
+// const startApp = async() => {
+//     const userRes = await readLineAsync("Enter list of age between 0 to 100 with comma(,) seperated : ");
+//     readline.close();
+//     const inputList = userRes.split(",");
+//     const FinalList = calcuAge(inputList);
+//     if (FinalList.length > 0) {
+//         console.log(calcuAge(inputList).toString);
+//     } else {
+//         console.log('NA');
+//     }
+// }
+
+// startApp();
+
+const AgeCalculator = (AgeList) => {
+    const FinalList = calcuAge(AgeList);
+    if (FinalList.length > 0) {
+        return FinalList.toString();
+    } else {
+        return 'NA';
+    }
 }
 
-const startApp = async() => {
-    const userRes = await readLineAsync("Enter list of age between 0 to 100 with comma(,) seperated : ");
-    readline.close();
-    const inputList = userRes.split(",");
-    const FinalList = calcuAge(inputList);
-    if(FinalList.length > 0){
-        console.log(calcuAge(inputList));
-    }else{
-        console.log('NA');
-    } 
-}
-
-startApp();
+const ageListInput = [1, 22, 55, 66, 88, 99, 6]
+console.log(AgeCalculator(ageListInput));
--- LuckyDaysolution/js/index.js
+++ LuckyDaysolution/js/index.js
@@ -3,8 +3,8 @@ const event = require('node:events');
 const fs = require('node:fs');
 const path = require('node:path');
 
-const hostname='127.0.0.1';
-const port=8081;
+const hostname = '127.0.0.1';
+const port = 8081;
 const myEventEmitter = new event.EventEmitter();
 const daymap = new Map();
 daymap.set(0, 'MONDAY');
@@ -15,48 +15,45 @@ daymap.set(4, 'FRIDAY');
 daymap.set(5, 'SATURDAY');
 daymap.set(6, 'SUNDAY');
 
-myEventEmitter.on('getLuckyDay',(luckyDay) => { 
-    luckyDay.day+=daymap.get(luckyDay.number%7)
+myEventEmitter.on('getLuckyDay', (luckyDay) => {
+    luckyDay.day += daymap.get(luckyDay.number % 7)
     console.log(luckyDay.day);
 });
 
-http.createServer((req,resp)=>{
+http.createServer((req, resp) => {
     console.log(req.url);
-    if(req.url === '/'){
-        filename=path.join(__dirname,'../views/index.html')
-        fs.readFile(filename,'utf-8',(err,content)=>{
-            if(err){
+    if (req.url === '/') {
+        filename = path.join(__dirname, '../views/index.html')
+        fs.readFile(filename, 'utf-8', (err, content) => {
+            if (err) {
                 resp.writeHead(500);
                 resp.end('Not able to load html view');
-            }
-            else {
+            } else {
                 // resp.writeHead(200,{"Content-Type":"text/css"});
                 // renderer.css(resp);
-                resp.writeHead(200,{"Content-Type":"text/html"});
+                resp.writeHead(200, { "Content-Type": "text/html" });
                 resp.end(content);
             }
         });
-    }
-    else if(req.method === 'POST' && req.url === '/getLucyDay') {
-        let result='';
+    } else if (req.method === 'POST' && req.url === '/getLucyDay') {
+        let result = '';
         console.log('inside post event');
-        req.on('data',(chunk)=>{
-            result+=chunk;
+        req.on('data', (chunk) => {
+            result += chunk;
         });
-        req.on('end',()=>{
-            console.log('Data received from client is - '+result)
-            
-            resp.writeHead(200,{"Content-Type":"text/html"});
-            const luckyDay = {number:parseInt(result),day:''};
-            myEventEmitter.emit('getLuckyDay',luckyDay);
-            console.log('finalOut-'+luckyDay.day);
+        req.on('end', () => {
+            console.log('Data received from client is - ' + result)
+
+            resp.writeHead(200, { "Content-Type": "text/html" });
+            const luckyDay = { number: parseInt(result), day: '' };
+            myEventEmitter.emit('getLuckyDay', luckyDay);
+            console.log('finalOut-' + luckyDay.day);
             resp.end(luckyDay.day);
         })
-    }
-    else {
+    } else {
         // resp.writableEnded(404);
         // resp.end('Page Not Found..!');
     }
-}).listen(port,hostname,()=>{
+}).listen(port, hostname, () => {
     console.log('Server is running up');
 });
